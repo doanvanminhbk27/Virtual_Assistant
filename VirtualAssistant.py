b5d8231854bdf57f2a87a8f196457e678ca2d29e
Văn Minh Đoàn <doanvanminhcd27@gmail.com>
2022-06-20 23:49:15 +0700

    20/6/2022

--- VirtualAssistant.py
+++ VirtualAssistant.py
@@ -4,29 +4,47 @@ from mypackage.Myfuntion import *
 from mypackage.Library import *
 from mypackage.Seach_Open import *
 
-
-speakEn("Can i help you")
-
-while(True):
-    h = hearEn()
-
+def Virtual(h):
+    global end_out  # global variable
     if "open" in h:
         open_web(h)
-
+        
     elif "for me video" in h:      
         Search_YouTube()
-
+    
     elif "what time" in h:
         whattime()
-
+    
     elif "today" in h:
         today()
 
-    
-
     elif "goodbye" in h or "thank you" in h:
         speakEn("goodbye minh doan, see you again")
-        break
+        end_out = 1
 
     else:
         speakEn("i dont understand, you can speak again")
+
+
+if __name__ == '__main__':
+    speakEn("Can i help you")
+    count_out = 0
+
+    while(True):
+        h = hearEn()
+        if(h):
+            Virtual(h)
+            count_out = 0
+            if(end_out == 1):
+                break
+            continue
+
+        else:
+            count_out += 1
+            if(count_out == 3):
+                time.sleep(2)
+                speakEn("Not request, good bye")
+                break
+            continue
+            
+
